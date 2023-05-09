c1a1c150a9d6398188f0318e8381bf555579127d
marh0819 <miguel.rivera01@uceva.edu.co>
2023-05-09 18:49:15 -0500

    Transformada de fourier funcional con 2 valores

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func comprobacion(lar int) bool {
 	x := false
@@ -125,6 +128,30 @@ func recursivaFourier2(entrada ...complex128) []complex128 {
 	}
 }
 
+func transformadaFormula(arre ...complex128) []complex128 {
+	n := len(arre)
+	w := complex(0, 0)
+	z := complex(0, 0)
+	salidaA := make([]complex128, n)
+
+	for m := 0; m < len(arre)/2; m += 2 {
+		aux := m / n
+		w = complex(float64(math.Cos(2*math.Pi*float64(aux))), imag(w))
+		w = complex(real(w), float64(-1*math.Sin(2*math.Pi*float64(aux))))
+		z = complex(real(w)*real(arre[m+1])-imag(w)*imag(arre[m+1]), imag(z))
+		z = complex(real(z), real(w)*imag(arre[m+1])+imag(w)*real(arre[m+1]))
+
+		salidaA[m] = complex(real(arre[m])+real(z), imag(salidaA[m]))
+		salidaA[m] = complex(real(salidaA[m]), imag(arre[m])+imag(z))
+
+		fmt.Println("", real(arre[m])-real(z))
+		salidaA[m+(n/2)] = complex(real(arre[m])-real(z), imag(salidaA[m+n/2]))
+		salidaA[m+(n/2)] = complex(real(salidaA[m+n/2]), imag(arre[m])-imag(z))
+
+	}
+	return salidaA
+}
+
 func main() {
 	//var entradaA = []int {}
 	//entradaA := make([]int , 0 )
@@ -157,6 +184,7 @@ func main() {
 	a := 8 // aqui van los multiplos de llenado malparido miguel
 	arreglo := []complex128{}
 	arreglo2 := []complex128{}
+	arreglo3 := []complex128{}
 
 	for k := 0; k < a; k++ {
 		aux := complex(float64(k), 0)
@@ -174,4 +202,6 @@ func main() {
 
 	// a = complex(3, imag(a))
 	fmt.Println(arreglo2, "Arreglo ")
+	arreglo3 = transformadaFormula(arreglo2[0], arreglo2[1])
+	fmt.Println(arreglo3)
 }
